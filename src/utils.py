25d9536d34e3e775ecab112ec11793f2e88d071d
an20805 <anshuman.40d@gmail.com>
2023-12-26 13:39:34 +0530

    prediction pipeline

--- src/utils.py
+++ src/utils.py
@@ -15,6 +15,14 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
     
+def load_object(file_path):
+    try:
+        with open(file_path, 'rb') as file_obj:
+            return dill.load(file_obj)
+    
+    except Exception as e:
+        raise CustomException(e, sys)
+    
 def evaluate_models(X_train, y_train,X_test,y_test,models):
     try:
         report = {}
