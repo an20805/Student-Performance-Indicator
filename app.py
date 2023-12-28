2a0f79b965a86f802b69e0e510420c530c78ea80
Anshuman Dangwal <115940865+an20805@users.noreply.github.com>
2023-12-28 19:02:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,15 +4,13 @@ import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from src.pipeline.predict_pipeline import CustomData, PredictPipeline
 
-app = Flask(__name__)
+application = Flask(__name__)
+
+app = application
 
 ## Route for home page
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/predictdata', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def predict_datapoint():
     if request.method=='GET':
         return render_template('home.html')
@@ -35,6 +33,7 @@ def predict_datapoint():
         return render_template('home.html', results=results[0])
     
 
-
+if __name__=='__main__':
+    app.run(host='0.0.0.0')
     
     
